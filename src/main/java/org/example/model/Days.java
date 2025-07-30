b4699075256b47cf58c5b5c9d78d0680c958e624
Syed Abbas <s.abbas@reply.com>
2025-07-30 12:30:40 +0100

    Added inital weather response model

---
+++ src/main/java/org/example/model/Days.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Days {
+    @JsonProperty("datetime")
+    private String datetime;
+
+    @JsonProperty("tempmax")
+    private int tempmax;
+
+    @JsonProperty("tempmin")
+    private int tempmin;
+
+    @JsonProperty("conditions")
+    private String conditions;
+}
